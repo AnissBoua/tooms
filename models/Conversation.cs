1f615cb9d50be69fcda1b9b621a767c6dd3c21cb
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2024-10-17 16:43:46 +0200

    Update Conversation.cs

--- models/Conversation.cs
+++ models/Conversation.cs
@@ -8,7 +8,7 @@ namespace tooms.models
     public class Conversation
     {
         public int Id { get; set; }
-        public List<User> Users { get; set; } = new List<User>();
+        public List<UserConversation> UserConversations { get; set; } = new List<UserConversation>();
         public List<Message> Messages { get; set; } = new List<Message>();
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
